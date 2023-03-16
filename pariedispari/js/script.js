c8647053f0ca34d981a4313d9dd120d3079cbdd0
simolorevg <simoneviaggi@libero.it>
2023-03-16 13:06:47 +0100

    Working on the script

---
+++ pariedispari/js/script.js
@@ -0,0 +1,5 @@
+let userChoice = prompt("Pari o Dispari");
+userChoice = userChoice.toLowerCase();
+console.log(userChoice);
+const userNumber = parseInt(prompt('Inserisci un numero: '));
+console.log(userNumber);
